f2fab8b0c71c844bb4a70ede787e1e0e912a8523
yotaishida <bohemia@yotaishidanoMacBook-Pro.local>
2023-10-03 20:55:21 +0900

    add net calorie

--- js/dg_function.js
+++ js/dg_function.js
@@ -4,7 +4,8 @@ function generate_mets_per_watt(weight){
     for (let watt = 0; watt <= 300; watt+=10){
          dict = { "watt": watt,
                   "mets":  ((watt * 12 + weight * 3.5)/3.5 /weight).toFixed(1),
-                  "calorie": ((watt * 12 + weight * 3.5)/3.5 *1.05).toFixed(0)
+                  "calorie": ((watt * 12 + weight * 3.5)/3.5 *1.05).toFixed(0),
+                  "net_calorie": ((watt * 12 )/3.5 *1.05).toFixed(0)
 
                 }
          ret.push(dict)
